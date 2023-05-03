dba6a4630f80160534377ae41407a365d00f1227
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2023-05-03 16:50:36 -0500

    Added timeouts to responses along with basic error checking

--- leetboost.py
+++ leetboost.py
@@ -1,27 +1,42 @@
 import tkinter as tk
 import requests
-import time
+from time import sleep
 
-init_response = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202").json()
-num_solved = init_response['totalSolved']
-new_num_solved = num_solved
-
-num_attempts = init_response['totalQuestions']
-new_num_attempts = num_attempts
-
+window = tk.Tk()
+window.title("LeetBoost")
 
 while True:
-    r = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202").json()
-    new_num_solved = r['totalSolved']
-    new_num_attempts = r['totalQuestions']
+    init_response = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=3)
+    if not init_response.ok:
+        print("API request failed, trying again")
+        # print(init_response.status_code)
+    else:
+        init_json = init_response.json()
+        num_solved = init_json['totalSolved']
+        new_num_solved = num_solved
 
-    user_input = input("press enter to continue")
+        num_attempts = init_json['totalQuestions']
+        new_num_attempts = num_attempts
 
-    if new_num_solved > num_solved:
-        num_solved = new_num_solved
-        print("Congrats on completing a problem!")
-    elif new_num_attempts > num_attempts:
-        num_attempts = new_num_attempts
-        print("Don't worry, you can do this! Don't give up! Take a deep breath and try again.")
 
-    time.sleep(5)
+        while window.winfo_exists():
+            r = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=3)
+            if not r.ok:
+                print("API request failed")
+                break
+            else:
+                r_json = r.json()
+                new_num_solved = r_json['totalSolved']
+                new_num_attempts = r_json['totalQuestions']
+
+                if new_num_solved > num_solved:
+                    num_solved = new_num_solved
+                    print("Congrats on completing a problem!")
+                elif new_num_attempts > num_attempts:
+                    num_attempts = new_num_attempts
+                    print("Don't worry, you can do this! Don't give up! Take a deep breath and try again.")
+            print("Everything good, waiting 2 seconds for next request")
+            sleep(2)
+            
+    print("Waiting 10 seconds before trying again")
+    sleep(10)
