88fcd04e91a20d26705a4963798d59d3bf86444a
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2023-05-04 14:41:11 -0500

    Fixed logic bug and removed magic numbers

--- leetboost.py
+++ leetboost.py
@@ -3,47 +3,45 @@ import requests
 from time import sleep
 import winsound
 
-# window = tk.Tk()
-# window.title("LeetBoost")
+
+# get first completion count
+timeout = 10  # seconds
+wait_time = 3  # seconds 
+
+init_response = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=timeout)
+num_solved = 0  # init
+attempts = 0
+while not init_response.ok and (attempts < 5):
+    print("API request failed. Trying", 5 - attempts, "more times")
+    attempts += 1
+    init_response = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=timeout)
+else:
+    init_json = init_response.json()
+    num_solved = init_json['totalSolved']
+    new_num_solved = num_solved
+
+    num_attempts = init_json['totalQuestions']
+    new_num_attempts = num_attempts
 
 
+# continuously check for a new completion
 while True:
-    init_response = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=3)
-    if not init_response.ok:
-        print("API request failed, trying again")
+    # while window.winfo_exists():
+    r = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=timeout)
+    if not r.ok:
+        print("API request failed")
     else:
-        init_json = init_response.json()
-        num_solved = init_json['totalSolved']
-        new_num_solved = num_solved
+        r_json = r.json()
+        new_num_solved = r_json['totalSolved']
+        new_num_attempts = r_json['totalQuestions']
 
-        num_attempts = init_json['totalQuestions']
-        new_num_attempts = num_attempts
-
-
-        # while window.winfo_exists():
-        r = requests.get("https://leetcode-stats-api.herokuapp.com/jpoland0202", timeout=3)
-        if not r.ok:
-            print("API request failed")
-            break
-        else:
-            r_json = r.json()
-            new_num_solved = r_json['totalSolved']
-            new_num_attempts = r_json['totalQuestions']
-
-            # testing_input = input("press enter or +")  # remove after testing
-            # if testing_input == "+":
-            #     new_num_solved += 1
-
-            if new_num_solved > num_solved:
-                num_solved = new_num_solved
-                print("Congrats on completing a problem!")
-                winsound.PlaySound("my_ding.wav", winsound.SND_FILENAME)
-            elif new_num_attempts > num_attempts:
-                num_attempts = new_num_attempts
-                print("Don't worry, you can do this! Don't give up! Take a deep breath and try again.")
-
-            # if testing_input == "+":  # remove after testing
-            #     num_solved -= 1
+        if new_num_solved > num_solved:
+            num_solved = new_num_solved
+            print("Congrats on completing a problem!")
+            winsound.PlaySound("my_ding.wav", winsound.SND_FILENAME)
+        elif new_num_attempts > num_attempts:
+            num_attempts = new_num_attempts
+            print("Don't worry, you can do this! Don't give up! Take a deep breath and try again.")
             
-    print("Pausing for 3 seconds")
-    sleep(3)
+    print(num_solved, "solutions detected. Will check again in", wait_time, "seconds.")
+    sleep(wait_time)
