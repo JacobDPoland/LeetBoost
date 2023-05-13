e06c7cd28688c66f22658c92ffc09b660aa3d6e1
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2023-05-13 10:02:53 -0500

    Updated test_sounds to play all sounds instead of just one

--- tests/test_sounds.py
+++ tests/test_sounds.py
@@ -2,11 +2,13 @@ import os
 import random
 import winsound
 
-sounds = os.listdir(os.getcwd() + "/sounds")
+sounds = os.listdir(os.getcwd() + "/../sounds")
 print("Available sounds:", sounds)
 
 # function for choosing and playing the sound effect
 def playSound():
     chosen_sound = random.choice(sounds)
-    winsound.PlaySound("sounds/" + chosen_sound, winsound.SND_FILENAME)
-playSound()
+    winsound.PlaySound("../sounds/" + chosen_sound, winsound.SND_FILENAME)
+
+for sound in sounds:
+    winsound.PlaySound("../sounds/" + sound, winsound.SND_FILENAME)
